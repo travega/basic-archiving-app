b501d4361ce9696f7bb70e1033c5f9ecfde7718c
Andrew Scott <andrewscott@andrewsc-ltm.internal.salesforce.com>
2018-07-23 13:45:59 +0100

    update log comment

--- archive_clock.rb
+++ archive_clock.rb
@@ -5,7 +5,7 @@ require 'rufus-scheduler'
 def self.send_archival_command
   db = Sequel.connect(ENV['DATABASE_URL'])
   db.run('select archive.run_archival()')
-  puts 'Archival taskrun!'
+  puts 'Archival task run!'
 end
 
 scheduler = Rufus::Scheduler.new
