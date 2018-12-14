6ac096b804637daf03ef1a357b3837a8d615d6ab
Andrew Scott <andrewscott@andrewsc-ltm.internal.salesforce.com>
2018-12-14 09:33:42 +0000

    update archiver

--- archiver.rb
+++ archiver.rb
@@ -1,19 +1,9 @@
 require 'sequel'
-# require 'rufus-scheduler'
 
 if  ENV['RACK_ENV'] == 'development'
   require 'dotenv/load'
 end
 
-# def self.send_archival_command
 db = Sequel.connect(ENV['DATABASE_URL'])
 db.run('select archive.run_archival()')
 puts 'Archival task run!'
-# end
-
-# scheduler = Rufus::Scheduler.new
-# scheduler.in '5s' do
-#   send_archival_command
-# end
-
-# scheduler.join
